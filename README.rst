e1dd914a988bf161b8c1dc6bf225a445c68fe9ef
Miguel Sanda <msanda@arrobalytics.com>
2018-10-30 08:44:25 -0400

    README RST & SETUP

---
+++ README.rst
@@ -0,0 +1,15 @@
+=============
+Django Ledger
+=============
+
+A bookkeeping & financial analysis app for the Django Framework.
+
+Django Ledger supports:
+
+- Chart of Accounts.
+- Financial Statements (Income Statement & Balance Sheets)
+- Entities (LLC, Corps, etc.)
+- General Ledgers
+- Journal Entries
+
+**THIS PACKAGE IS UNDER ACTIVE DEVELOPMENT AND IS NOT SUITABLE FOR PRODUCTION**
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='django-ledger',
-    version='0.0.1',
+    version='0.0.2',
     packages=['django_ledger', 'django_ledger.models', 'django_ledger.models.io',
               'django_ledger.models.mixins', 'django_ledger.migrations'],
     url='http://djangoledger.io',
     license='MIT',
     author='Miguel Sanda',
     author_email='msanda@arrobalytics.com',
-    description='Financial analysis backend for Django'
+    description='Financial analysis backend for Django. Balance Sheet, Income Statements, Chart of Accounts, Entities',
 )
