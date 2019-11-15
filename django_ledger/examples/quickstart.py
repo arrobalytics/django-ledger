f8d50823b125e9b4bdd9aab778bbc5f0c63e0445
Miguel Sanda <msanda@arrobalytics.com>
2019-11-14 21:04:58 -0500

    Optimizations ...

--- django_ledger/examples/quickstart.py
+++ django_ledger/examples/quickstart.py
@@ -3,7 +3,6 @@ from django_ledger.models.coa import ChartOfAccountModel, make_account_active, g
 from django_ledger.models.coa_default import CHART_OF_ACCOUNTS
 from django_ledger.models.entity import EntityModel
 from django_ledger.models.utils import create_coa_structure
-from django_ledger.models.accounts import ACCOUNT_ROLES
 
 RECREATE_COA = False
 
@@ -11,18 +10,16 @@ if RECREATE_COA:
     EntityModel.objects.all().delete()
     ChartOfAccountModel.objects.all().delete()
     AccountModel.objects.all().delete()
-    create_coa_structure(coa_data=CHART_OF_ACCOUNTS, coa_name='CoA Default')
+    coa = create_coa_structure(coa_data=CHART_OF_ACCOUNTS, coa_name='CoA QuickStart')
+    make_account_active(coa, ['1010', '3010', '1610', '2110', '6253', '4020'])
 
-coa, created = ChartOfAccountModel.objects.get_or_create(slug='coa-default')
-make_account_active(coa, ['1010', '3010', '1610', '2110', '6253', '4020'])
-
-idx = get_acc_idx(coa, as_dataframe=0)
+coa, created = ChartOfAccountModel.objects.get_or_create(name='CoA QuickStart')
 
 company, created = EntityModel.objects.get_or_create(slug='my-co-inc',
                                                      coa=coa,
                                                      name='MyCo Inc')
 
-ledger_id = 'my-debug-ledger-66387'
+ledger_id = 'my-co-ledger'  # auto generated if not provided
 myco_ledger, created = company.general_ledger.get_or_create(slug=ledger_id, name='My Debug Ledger')
 
 myco_ledger.journal_entry.all().delete()
@@ -35,7 +32,7 @@ myco_ledger.tx_generic(
     debit_acc='1010',
     credit_acc='3010',
     activity='other',
-    desc='Company funding to buy real estate.'
+    desc='Company funding.'
 )
 
 # Buy property
@@ -45,7 +42,7 @@ myco_ledger.tx_generic(
     debit_acc='1610',
     credit_acc='1010',
     activity='inv',
-    desc='Company funding to buy real estate.'
+    desc='Real estate down payment'
 )
 
 # Funding Company ---
@@ -55,7 +52,7 @@ myco_ledger.tx_generic(
     debit_acc='1610',
     credit_acc='2110',
     activity='fin',
-    desc='Company funding to buy real estate.'
+    desc='Mortgage to buy real estate.'
 )
 
 # An expense ----
@@ -79,7 +76,5 @@ myco_ledger.tx_generic(
 )
 
 # Balance Sheet & Income Statement ----
-bs = myco_ledger.balance_sheet(as_dataframe=True)
+bs = myco_ledger.balance_sheet(as_dataframe=True, signs=0)
 ic = myco_ledger.income_statement(as_dataframe=True, signs=True)
-
-myco_ledger.get_coa()
