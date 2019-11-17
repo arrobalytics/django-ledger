197822edfee0e46080c5a6630ed07c91e9000c04
Miguel Sanda <msanda@arrobalytics.com>
2019-11-16 22:17:17 -0500

    JEs Engine Optimization

--- django_ledger/examples/quickstart.py
+++ django_ledger/examples/quickstart.py
@@ -4,78 +4,97 @@ from django_ledger.models.coa_default import CHART_OF_ACCOUNTS
 from django_ledger.models.entity import EntityModel
 from django_ledger.models.utils import create_coa_structure
 
-RESET_DATABASE = True
 
-# USE WITH CAUTION!!!!, WILL DELETE ENTIRE DATABASE.
-if RESET_DATABASE:
-    EntityModel.objects.all().delete()
-    ChartOfAccountModel.objects.all().delete()
-    AccountModel.objects.all().delete()
-    coa = create_coa_structure(coa_data=CHART_OF_ACCOUNTS, coa_name='CoA QuickStart')
-    make_account_active(coa, ['1010', '3010', '1610', '2110', '6253', '4020'])
+def quickstart(reset_db=False):
+    """
+    Django Ledger QuickStart Function
+    :param reset_db: USE WITH CAUTION!!!!, WILL DELETE ENTIRE DATABASE.
+    :return:
+    """
+    if reset_db:
+        EntityModel.objects.all().delete()
+        ChartOfAccountModel.objects.all().delete()
+        AccountModel.objects.all().delete()
+        coa = create_coa_structure(coa_data=CHART_OF_ACCOUNTS, coa_name='CoA QuickStart')
+        make_account_active(coa, ['1010', '3010', '1610', '2110', '6253', '6290', '4020'])
 
-coa, created = ChartOfAccountModel.objects.get_or_create(name='CoA QuickStart')
+    coa, created = ChartOfAccountModel.objects.get_or_create(name='CoA QuickStart')
 
-company, created = EntityModel.objects.get_or_create(slug='my-co-inc',
-                                                     coa=coa,
-                                                     name='MyCo Inc')
+    company, created = EntityModel.objects.get_or_create(slug='my-co-inc',
+                                                         coa=coa,
+                                                         name='MyCo Inc')
 
-ledger_id = 'my-co-ledger'  # auto generated if not provided
-myco_ledger, created = company.general_ledger.get_or_create(slug=ledger_id, name='My Debug Ledger')
+    ledger_id = 'my-co-ledger'  # auto generated if not provided
+    myco_ledger, created = company.general_ledger.get_or_create(slug=ledger_id, name='My Debug Ledger')
 
-myco_ledger.journal_entry.all().delete()
+    myco_ledger.journal_entry.all().delete()
 
-# Company Funding
-funding = 200000
-myco_ledger.tx_generic(
-    amount=funding,
-    start_date='2019-10-02',
-    debit_acc='1010',
-    credit_acc='3010',
-    activity='other',
-    desc='Company funding.'
-)
+    # Company Funding
+    funding = 200000
+    myco_ledger.tx_generic(
+        amount=funding,
+        start_date='2019-10-02',
+        debit_acc='1010',
+        credit_acc='3010',
+        activity='other',
+        desc='Company funding.'
+    )
 
-# Buy property
-myco_ledger.tx_generic(
-    amount=40000,
-    start_date='2019-10-02',
-    debit_acc='1610',
-    credit_acc='1010',
-    activity='inv',
-    desc='Real estate down payment'
-)
+    # Buy property
+    myco_ledger.tx_generic(
+        amount=40000,
+        start_date='2019-10-02',
+        debit_acc='1610',
+        credit_acc='1010',
+        activity='inv',
+        desc='Real estate down payment'
+    )
 
-# Funding Company ---
-myco_ledger.tx_generic(
-    amount=80000,
-    start_date='2019-10-02',
-    debit_acc='1610',
-    credit_acc='2110',
-    activity='fin',
-    desc='Mortgage to buy real estate.'
-)
+    # Funding Company ---
+    myco_ledger.tx_generic(
+        amount=80000,
+        start_date='2019-10-02',
+        debit_acc='1610',
+        credit_acc='2110',
+        activity='fin',
+        desc='Mortgage to buy real estate.'
+    )
 
-# An expense ----
-myco_ledger.tx_generic(
-    amount=100,
-    start_date='2019-11-02',
-    debit_acc='6253',
-    credit_acc='1010',
-    activity='op',
-    desc='HOA Expenses Nov 2019'
-)
+    # An expense ----
+    myco_ledger.tx_generic(
+        amount=100,
+        start_date='2019-11-02',
+        debit_acc='6253',
+        credit_acc='1010',
+        activity='op',
+        desc='HOA Expenses Nov 2019'
+    )
 
-# An Income ----
-myco_ledger.tx_generic(
-    amount=1200,
-    start_date='2019-11-06',
-    debit_acc='1010',
-    credit_acc='4020',
-    activity='op',
-    desc='HOA Expenses Nov 2019'
-)
+    # An Income ----
+    myco_ledger.tx_generic(
+        amount=1200,
+        start_date='2019-11-06',
+        debit_acc='1010',
+        credit_acc='4020',
+        activity='op',
+        desc='HOA Expenses Nov 2019'
+    )
 
-# Balance Sheet & Income Statement ----
-bs = myco_ledger.balance_sheet(as_dataframe=True, signs=0)
-ic = myco_ledger.income_statement(as_dataframe=True, signs=True)
+    # Another Expense ----
+    myco_ledger.tx_generic(
+        amount=115.50,
+        start_date='2019-12-22',
+        debit_acc='6290',
+        credit_acc='1010',
+        activity='op',
+        desc='November Electricity Bill'
+    )
+
+    # Balance Sheet & Income Statement ----
+    bs = myco_ledger.balance_sheet(as_dataframe=True, signs=0)
+    bs_f = myco_ledger.balance_sheet(as_dataframe=False, signs=0)
+    ic = myco_ledger.income_statement(as_dataframe=True, signs=True)
+    return bs, bs_f, ic
+
+
+# bs, bs_f, ic = quickstart(reset_db=False)
