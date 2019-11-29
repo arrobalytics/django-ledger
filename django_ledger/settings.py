e119e3ab3311d63c8a30fcdd70db30d086558fcc
Miguel Sanda <msanda@arrobalytics.com>
2019-11-28 22:52:22 -0500

    Settings...

---
+++ django_ledger/settings.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+
+USER_SETTINGS = getattr(settings, 'DJANGO_LEDGER_SETTINGS', dict())
+
+DJANGO_LEDGER_SETTINGS = {
+    'ACCOUNT_MAX_LENGTH': USER_SETTINGS.get('ACCOUNT_MAX_LENGTH', 5)
+}
