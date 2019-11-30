7c7458c5e41489fb827e6a6058dc156e401df341
Miguel Sanda <msanda@arrobalytics.com>
2019-11-30 00:45:34 -0500

    CoA View

--- django_ledger/urls.py
+++ django_ledger/urls.py
@@ -7,4 +7,9 @@ app_name = 'django_ledger'
 urlpatterns = [
     path('entity/', views.EntityModelListView.as_view(), name='entity-list'),
     path('entity/<slug:entity_slug>/', views.EntityModelDetailVew.as_view(), name='entity-detail'),
+
+    path('coa/<slug:coa_slug>', views.ChartOfAccountsDetailView.as_view(), name='coa-detail'),
+
+    path('entity/<slug:entity_slug>/balance-sheet/', views.EntityModelDetailVew.as_view(), name='entity-bs'),
+    path('entity/<slug:entity_slug>/income-statement/', views.EntityModelDetailVew.as_view(), name='entity-ic'),
 ]
