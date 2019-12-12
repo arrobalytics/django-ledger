72868b50624540e11dd5586a23a6173d3317825a
Miguel Sanda <msanda@arrobalytics.com>
2019-12-11 22:03:24 -0500

    TypeO Fix

--- django_ledger/models/__init__.py
+++ django_ledger/models/__init__.py
@@ -2,5 +2,5 @@ from django_ledger.models.accounts import AccountModel
 from django_ledger.models.coa import ChartOfAccountModel
 from django_ledger.models.entity import EntityModel, EntityManagementModel
 from django_ledger.models.journalentry import JournalEntryModel
-from django_ledger.models.ledger import LedgerModel, IOMixIn
+from django_ledger.models.ledger import LedgerModel
 from django_ledger.models.transactions import TransactionModel
