382cf46bb902c64178e140a5024c29214f1c7046
Miguel Sanda <msanda@arrobalytics.com>
2020-04-21 22:23:49 -0400

    webpack.config.js update Removed compiled less files.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -20,6 +20,25 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            lessOptions: {
+                                strictMath: true,
+                            },
+                        },
+                    },
+                ],
+            },
+            {
                 test: /\.css$/i,
                 use: [
                     'style-loader',
@@ -38,12 +57,6 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: [
                     "ts-loader"
-                ],
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    "less-loader"
                 ],
             },
             {
