cda94046a954c7a0011f99a349f3381b92aaec7f
Miguel Sanda <msanda@arrobalytics.com>
2020-06-14 20:13:57 -0400

    v0.2.11 packaging bugfixes

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ import django_ledger
 setup(
     name='django-ledger',
     version=django_ledger.__version__,
-    packages=find_packages(),
+    packages=find_packages(exclude=['djltest']),
     url=django_ledger.__url__,
     license=django_ledger.__license__,
     keywords='django, finance, accounting, balance sheet, income statement, general ledger, money, engine',
     author=django_ledger.__author__,
     author_email=django_ledger.__email__,
     description='Financial analysis backend for Django. Balance Sheet, Income Statements, Chart of Accounts, Entities',
+    include_package_data=True,
     install_requires=[
         'django',
         'django-mptt'
