56d0c186c77a05d92ea6def63d7863766e6cf60f
elarroba <msanda@arrobalytics.com>
2020-11-24 21:08:10 -0500

    Ledger Y/Q/M BaS/IC views & templates

--- django_ledger/urls/ledger.py
+++ django_ledger/urls/ledger.py
@@ -5,10 +5,4 @@ urlpatterns = [
     path('<slug:entity_slug>/list/', views.LedgerModelListView.as_view(), name='ledger-list'),
     path('<slug:entity_slug>/create/', views.LedgerModelCreateView.as_view(), name='ledger-create'),
     path('<slug:entity_slug>/update/<uuid:ledger_pk>/', views.LedgerModelUpdateView.as_view(), name='ledger-update'),
-    path('<slug:entity_slug>/balance-sheet/<uuid:ledger_pk>/',
-         views.LedgerBalanceSheetView.as_view(),
-         name='ledger-bs'),
-    path('<slug:entity_slug>/income-statement/<uuid:ledger_pk>/',
-         views.LedgerIncomeStatementView.as_view(),
-         name='ledger-ic'),
 ]
