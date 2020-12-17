01bf97749a952c6500005cdd2f391149f0450a1d
michaelnoel <noel.michael87@gmail.com>
2020-12-17 13:45:24 -0600

    transaction form starts with je_pk as it's initial value

--- django_ledger/forms/transactions.py
+++ django_ledger/forms/transactions.py
@@ -1,3 +1,12 @@
+"""
+Django Ledger created by Miguel Sanda <msanda@arrobalytics.com>.
+Copyright© EDMA Group Inc licensed under the GPLv3 Agreement.
+
+Contributions to this module:
+Miguel Sanda <msanda@arrobalytics.com>
+Michael Noel <noel.michael87@gmail.com>
+"""
+
 from django.forms import ModelForm, modelformset_factory, BaseModelFormSet, TextInput, Select, HiddenInput
 
 from django_ledger.io import validate_tx_data
@@ -56,12 +65,9 @@ class BaseTransactionModelFormSet(BaseModelFormSet):
             user_model=self.USER_MODEL
         )
 
-        if je_pk:
-            je_qs = je_qs.filter(uuid__exact=self.JE_PK)
-
         for form in self.forms:
             form.fields['account'].queryset = account_qs
-            form.fields['journal_entry'].queryset = je_qs
+            form.fields['journal_entry'].initial = self.JE_PK
 
     def clean(self):
         if any(self.errors):
