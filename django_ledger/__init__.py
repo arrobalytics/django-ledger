ba3f4336ab38088f2c862d69908fd74ba7f169c0
elarroba <msanda@arrobalytics.com>
2020-12-30 09:51:23 -0500

    v0.4.0b0 Version update

--- django_ledger/__init__.py
+++ django_ledger/__init__.py
@@ -9,7 +9,7 @@ Miguel Sanda <msanda@arrobalytics.com>
 default_app_config = 'django_ledger.apps.DjangoLedgerConfig'
 
 """Django Ledger"""
-__version__ = '0.3.28'
+__version__ = '0.4.0b0'
 __license__ = 'GPLv3 License'
 
 __author__ = 'Miguel Sanda'
