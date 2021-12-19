5d138e2ed49349a7ebe15b677ad4634e6c58aa41
elarroba <msanda@arrobalytics.com>
2021-12-19 13:17:04 -0500

    Bill Detail tests. Pipfile update

--- django_ledger/tests/test_bill.py
+++ django_ledger/tests/test_bill.py
@@ -24,10 +24,14 @@ class BillModelTests(DjangoLedgerBaseTest):
             p.name: set(p.pattern.converters.keys()) for p in bill_urls
         }
 
-    def create_bill(self, amount: Decimal, date: date = None) -> tuple[EntityModel, BillModel]:
+    def create_bill(self, amount: Decimal, date: date = None, is_accrued: bool = False) -> tuple[
+        EntityModel, BillModel]:
         entity_model: EntityModel = choice(self.ENTITY_MODEL_QUERYSET)
         vendor_model: VendorModel = entity_model.vendors.first()
-        account_qs = entity_model.coa.accounts.all()
+        account_qs = entity_model.get_accounts(
+            user_model=self.user_model
+        )
+
         len(account_qs)  # force evaluation
 
         cash_account = account_qs.filter(role__in=[ASSET_CA_CASH]).first()
@@ -44,6 +48,7 @@ class BillModelTests(DjangoLedgerBaseTest):
         bill_model.amount_due = amount
         bill_model.date = dt
         bill_model.vendor = vendor_model
+        bill_model.accrue = is_accrued
         bill_model.xref = 'ABC123xref'
         bill_model.cash_account = cash_account
         bill_model.prepaid_account = prepaid_account
@@ -273,7 +278,8 @@ class BillModelTests(DjangoLedgerBaseTest):
         }
 
         create_response = self.CLIENT.post(bill_create_url, data=bill_data, follow=True)
-        self.assertFormError(create_response, form='form', field=None, errors=['Must provide a cash account.'])
+        self.assertFormError(create_response, form='form', field=None,
+                             errors=['Must provide a cash account.'])
 
         bill_data['cash_account'] = account_qs.with_roles(roles=ASSET_CA_CASH).first().uuid
         create_response = self.CLIENT.post(bill_create_url, data=bill_data, follow=True)
@@ -297,8 +303,90 @@ class BillModelTests(DjangoLedgerBaseTest):
         create_response = self.CLIENT.post(bill_create_url, data=bill_data, follow=True)
         self.assertTrue(create_response.resolver_match.view_name, 'django_ledger:bill-detail')
 
-    # def test_bill_detail(self):
-    #     today = localdate()
-    #
-    #     for i in range(5):
-    #         entity_model, bill_model = self.create_bill(amount=Decimal('3000.00'), date=today)
+    def test_bill_detail(self):
+        self.login_client()
+        today = localdate()
+
+        for i in range(5):
+            entity_model, bill_model = self.create_bill(amount=Decimal('0.00'), date=today)
+            vendor_model: VendorModel = bill_model.vendor
+            bill_detail_url = reverse('django_ledger:bill-detail',
+                                      kwargs={
+                                          'entity_slug': entity_model.slug,
+                                          'bill_pk': bill_model.uuid
+                                      })
+
+            with self.assertNumQueries(8):
+                bill_detail_response = self.CLIENT.get(bill_detail_url)
+            self.assertTrue(bill_detail_response.status_code, 200)
+
+            self.assertTrue(bill_model.is_draft())
+            # 'Not Approved' is displayed to the user...
+            self.assertFalse(bill_model.is_approved())
+
+            # bill card is displayed to the user...
+            self.assertContains(bill_detail_response, 'id="djl-bill-card-widget"')
+
+            # vendor card is displayed to the user...
+            self.assertContains(bill_detail_response, 'id="djl-vendor-card-widget"')
+
+            # if bill is not approved or draft...
+            # user can update bill
+            self.assertContains(bill_detail_response, 'id="djl-bill-detail-update-button"')
+            # user cannot mark as paid
+            self.assertNotContains(bill_detail_response, 'id="djl-bill-detail-mark-paid-button"')
+            # user can delete..
+            self.assertContains(bill_detail_response, 'id="djl-bill-detail-delete-button"')
+            # user cannot void...
+            self.assertNotContains(bill_detail_response, 'id="djl-bill-detail-void-button"')
+
+            # user can navigate to bill-list...
+            self.assertContains(bill_detail_response,
+                                reverse('django_ledger:bill-list',
+                                        kwargs={
+                                            'entity_slug': entity_model.slug
+                                        }))
+
+            # vendor name ia shown
+            self.assertContains(bill_detail_response, vendor_model.vendor_name)
+
+            # can edit vendor link
+            self.assertContains(bill_detail_response,
+                                reverse('django_ledger:vendor-update',
+                                        kwargs={
+                                            'entity_slug': entity_model.slug,
+                                            'vendor_pk': vendor_model.uuid
+                                        }))
+
+            # link to cash account detail
+            self.assertContains(bill_detail_response,
+                                reverse('django_ledger:account-detail',
+                                        kwargs={
+                                            'entity_slug': entity_model.slug,
+                                            'account_pk': bill_model.cash_account.uuid
+                                        }))
+
+            # amount paid is shown
+            self.assertContains(bill_detail_response, 'id="djl-bill-detail-amount-paid"')
+
+            # amount owed is shown
+            self.assertContains(bill_detail_response, 'id="djl-bill-detail-amount-owed"')
+
+            if not bill_model.accrue:
+                # amount prepaid is not shown
+                self.assertNotContains(bill_detail_response, ' id="djl-bill-detail-amount-prepaid"')
+                # amount unearned is not shown
+                self.assertNotContains(bill_detail_response, ' id="djl-bill-detail-amount-unearned"')
+
+            else:
+                # amount prepaid is shown
+                self.assertContains(bill_detail_response, ' id="djl-bill-detail-amount-prepaid"')
+                # amount unearned is shown
+                self.assertContains(bill_detail_response, ' id="djl-bill-detail-amount-unearned"')
+
+            # amounts are zero...
+            self.assertEqual(bill_model.get_amount_cash(), Decimal('0.00'))
+            self.assertEqual(bill_model.get_amount_earned(), Decimal('0.00'))
+            self.assertEqual(bill_model.get_amount_open(), Decimal('0.00'))
+            self.assertEqual(bill_model.get_amount_prepaid(), Decimal('0.00'))
+            self.assertEqual(bill_model.get_amount_unearned(), Decimal('0.00'))
