c6380548e422366906885ac3d3ecf87cb3815c60
Eric paul <erickimosavej@gmail.com>
2022-03-13 13:01:46 +0300

    new schema and a readme file
    new schema i.e. Bank account, accounts and a readme file for getting started with Django ledger graphql api

--- django_ledger/contrib/django_ledger_graphql/api.py
+++ django_ledger/contrib/django_ledger_graphql/api.py
@@ -4,6 +4,8 @@ from django.utils.functional import SimpleLazyObject
 
 from .customers.schema import CustomerQuery
 from django_ledger.contrib.django_ledger_graphql.bill.schema import Bill_list_Query
+from django_ledger.contrib.django_ledger_graphql.accounts.schema import Accountlist_Query
+from django_ledger.contrib.django_ledger_graphql.bank_account.schema import Bank_account_Query
 
 API_PATH = SimpleLazyObject(lambda: reverse("api"))
 
@@ -11,6 +13,8 @@ API_PATH = SimpleLazyObject(lambda: reverse("api"))
 class Query(
     CustomerQuery,
     Bill_list_Query,
+    Accountlist_Query,
+    Bank_account_Query,
 ):
     pass
 
