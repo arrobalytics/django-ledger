005f6cdf296facdbbc02fc1c9cc34a7321ddac1e
elarroba <msanda@arrobalytics.com>
2022-03-13 17:40:55 -0400

    Customer job card & form development.

--- dev_env/urls.py
+++ dev_env/urls.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 
-from graphene_django.views import GraphQLView
-from django_ledger.contrib.django_ledger_graphql.api import schema
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django_ledger.urls', namespace='django_ledger')),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
+
+try:
+    # checking for graphene_django installation to provide and enable graphql services...
+    from graphene_django.views import GraphQLView
+    from django_ledger.contrib.django_ledger_graphql.api import schema
+
+    urlpatterns += [
+        path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)))
+    ]
+except ImportError:
+    pass
