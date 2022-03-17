a303d2ca4c345a3500e6fdcb1e72fd7b92b4d520
Eric paul <erickimosavej@gmail.com>
2022-03-17 20:03:35 +0300

    changed schemas for pagination and filtering fuctionalities

--- django_ledger/contrib/django_ledger_graphql/coa/schema.py
+++ django_ledger/contrib/django_ledger_graphql/coa/schema.py
@@ -1,18 +1,25 @@
 
 import graphene
+from graphene import relay
 from graphene_django import DjangoObjectType
 from django_ledger.models import ChartOfAccountModel
+from graphene_django.filter import DjangoFilterConnectionField
 
-
-class CoaList(DjangoObjectType):
+class CoaNode(DjangoObjectType):
     class Meta:
         model = ChartOfAccountModel
+        filter_fields = {
+            'slug' : ['exact', 'icontains', 'istartswith'],
+            'name' : ['exact', 'icontains', 'istartswith'],
+            'description' : ['exact', 'icontains', 'istartswith']
+        }
+        interfaces = (relay.Node,)
 
 
 class ChartOfAccountsQuery(graphene.ObjectType):
-    all_coa = graphene.List(CoaList, slug_name=graphene.String(required=True))
+    all_coa = DjangoFilterConnectionField(CoaNode, slug_name=graphene.String(required=True))
 
-    def resolve_all_coa(self, info, slug_name):
+    def resolve_all_coa(self, info, slug_name, **kwargs):
         if info.context.user.is_authenticated:
             return ChartOfAccountModel.objects.for_entity(
                 entity_slug=slug_name,
--- django_ledger/contrib/django_ledger_graphql/item/schema.py
+++ django_ledger/contrib/django_ledger_graphql/item/schema.py
@@ -1,19 +1,22 @@
-
 import graphene
+from graphene import relay
 from graphene_django import DjangoObjectType
 from django_ledger.models import UnitOfMeasureModel
+from graphene_django.filter import DjangoFilterConnectionField
 
-
-class UnitOfMeasureList(DjangoObjectType):
+class UnitOfMeasureNode(DjangoObjectType):
     class Meta:
         model = UnitOfMeasureModel
+        filter_fields = {
+            'name' : ['exact', 'icontains', 'istartswith'],
+            'unit_abbr': ['exact', 'icontains', 'istartswith'],
+            'is_active' : ['exact']
+        }
+        interfaces = (relay.Node,)
 
 
 class UnitOfMeasureQuery(graphene.ObjectType):
-    all_unit_of_measure = graphene.List(
-        UnitOfMeasureList,
-        slug_name=graphene.String(
-            required=True))
+    all_unit_of_measure = DjangoFilterConnectionField(UnitOfMeasureNode, slug_name=graphene.String(required=True))
 
     def resolve_all_unit_of_measure(self, info, slug_name):
         if info.context.user.is_authenticated:
