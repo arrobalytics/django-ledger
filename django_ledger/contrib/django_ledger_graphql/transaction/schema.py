c1a0148d5faec0dc79411341540af2a0dc78dbfc
Eric paul <erickimosavej@gmail.com>
2022-03-17 20:10:13 +0300

    new schemas

---
+++ django_ledger/contrib/django_ledger_graphql/transaction/schema.py
@@ -0,0 +1,33 @@
+import graphene
+from graphene import relay
+from graphene_django import DjangoObjectType
+from django_ledger.models import TransactionModel
+from graphene_django.filter import DjangoFilterConnectionField
+
+
+class TransactionNode(DjangoObjectType):
+    class Meta:
+        model = TransactionModel
+        filter_fields = {
+            'journal_entry': ['exact'],
+            'account': ['exact'],
+            'tx_type': ['exact', 'icontains', 'istartswith'],
+            'amount': ['exact', 'icontains', 'istartswith'],
+            'description': ['exact', 'icontains', 'istartswith'],
+        }
+        interfaces = (relay.Node,)
+class TransactionsQuery(graphene.ObjectType):
+    all_transactions = DjangoFilterConnectionField(TransactionNode, slug_name=graphene.String(required=True),
+     pk_je=graphene.UUID(), pk_ledger=graphene.UUID())
+
+    def resolve_all_transactions(self, info, slug_name, pk_je, pk_ledger, **kwargs):
+        if info.context.user.is_authenticated:
+            return TransactionModel.objects.for_journal_entry(
+                entity_slug=slug_name,
+                user_model=info.context.user,
+                je_pk=pk_je,
+                ledger_pk=pk_ledger
+            ).order_by('account__code')
+        else:
+            return TransactionModel.objects.none()
+
---
+++ django_ledger/contrib/django_ledger_graphql/unit/schema.py
@@ -0,0 +1,26 @@
+import graphene
+from graphene import relay
+from graphene_django import DjangoObjectType
+from django_ledger.models import EntityUnitModel
+from graphene_django.filter import DjangoFilterConnectionField
+
+
+class EntityUnitNode(DjangoObjectType):
+    class Meta:
+        model = EntityUnitModel
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+        }
+        interfaces = (relay.Node,)
+class EntityUnitQuery(graphene.ObjectType):
+    all_entity_unit = DjangoFilterConnectionField(EntityUnitNode, slug_name=graphene.String(required=True))
+
+    def resolve_all_vendors(self, info, slug_name, **kwargs):
+        if info.context.user.is_authenticated:
+            return EntityUnitModel.objects.for_entity(
+            entity_slug=slug_name,
+            user_model=info.context.user
+        )
+        else:
+            return EntityUnitModel.objects.none()
+
