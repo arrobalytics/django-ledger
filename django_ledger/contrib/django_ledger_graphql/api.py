2d0c9f4eea39a52afabd684eb0930b5ecf848844
Eric paul <erickimosavej@gmail.com>
2022-04-12 15:57:21 +0300

    updated README

--- django_ledger/contrib/django_ledger_graphql/api.py
+++ django_ledger/contrib/django_ledger_graphql/api.py
@@ -5,6 +5,7 @@ from django.utils.functional import SimpleLazyObject
 from .customers.schema import CustomerQuery
 from .customers.mutations import CustomerMutations
 from .auth.mutations import AuthMutation
+from .auth.schema import QueryUser
 from .bank_account.mutations import BankAccountMutations
 from django_ledger.contrib.django_ledger_graphql.bill.schema import Bill_list_Query
 from django_ledger.contrib.django_ledger_graphql.accounts.schema import Accountlist_Query
@@ -37,6 +38,7 @@ class Query(
     TransactionsQuery,
     JournalEntryQuery,
     PurchaseOrderQuery,
+    QueryUser,
 
 ):
     pass
