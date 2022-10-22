5ca5aa6e4e571e0553c420beaeb6b92fbba73209
elarroba <msanda@arrobalytics.com>
2022-10-22 10:53:29 -0400

    Implementation of Customer & Vendor Numbers.

--- django_ledger/models/customer.py
+++ django_ledger/models/customer.py
@@ -9,12 +9,15 @@ Pranav P Tulshyan <ptulshyan77@gmail.com>
 
 from uuid import uuid4
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db import models
+from django.db import models, transaction
 from django.utils.translation import gettext_lazy as _
 
 from django_ledger.models.mixins import ContactInfoMixIn, CreateUpdateMixIn, SlugNameMixIn, TaxCollectionMixIn
 from django.db.models import Q
+from django_ledger.models.utils import LazyLoader
+from django_ledger.settings import DJANGO_LEDGER_DOCUMENT_NUMBER_PADDING, DJANGO_LEDGER_CUSTOMER_NUMBER_PREFIX
 
 """
 The model for managing the details of the Customers.
@@ -22,6 +25,8 @@ For raising an Invoice ,we have to select the Customer Name.
 In case, the customer is not created , then the same needs to be created under this table.
 
 """
+
+lazy_loader = LazyLoader()
 
 
 class CustomerModelManager(models.Manager):
@@ -72,6 +77,7 @@ class CustomerModel(ContactInfoMixIn, TaxCollectionMixIn, CreateUpdateMixIn):
 
     uuid = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     customer_name = models.CharField(max_length=100)
+    customer_number = models.CharField(max_length=30, null=True, blank=True)
     entity = models.ForeignKey('django_ledger.EntityModel',
                                editable=False,
                                on_delete=models.CASCADE,
@@ -94,8 +100,59 @@ class CustomerModel(ContactInfoMixIn, TaxCollectionMixIn, CreateUpdateMixIn):
             models.Index(fields=['hidden']),
         ]
         unique_together = [
-            ('entity', 'customer_name')
+            ('entity', 'customer_number')
         ]
 
     def __str__(self):
         return f'Customer: {self.customer_name}'
+
+    def generate_customer_number(self, commit: bool = False) -> str:
+        """
+        Atomic Transaction. Generates the next Customer Number available.
+        @param commit: Commit transaction into CustomerModel.
+        @return: A String, representing the current InvoiceModel instance Document Number.
+        """
+        if not self.customer_number:
+            with transaction.atomic():
+
+                EntityStateModel = lazy_loader.get_entity_state_model()
+
+                try:
+                    LOOKUP = {
+                        'entity_id__exact': self.entity_id,
+                        'key__exact': EntityStateModel.KEY_CUSTOMER
+                    }
+
+                    state_model_qs = EntityStateModel.objects.select_related('entity').filter(**LOOKUP)
+                    state_model = state_model_qs.get()
+                    state_model.sequence += 1
+                    state_model.save(update_fields=['sequence'])
+
+                except ObjectDoesNotExist:
+                    EntityModel = lazy_loader.get_entity_model()
+
+                    LOOKUP = {
+                        'entity_id': self.entity_id,
+                        'key': EntityStateModel.KEY_CUSTOMER,
+                        'sequence': 1
+                    }
+                    state_model = EntityStateModel(**LOOKUP)
+                    state_model.clean()
+                    state_model.save()
+
+            seq = str(state_model.sequence).zfill(DJANGO_LEDGER_DOCUMENT_NUMBER_PADDING)
+            self.customer_number = f'{DJANGO_LEDGER_CUSTOMER_NUMBER_PREFIX}-{seq}'
+
+            if commit:
+                self.save(update_fields=['customer_number'])
+
+        return self.customer_number
+
+    def clean(self):
+        if not self.customer_number:
+            self.generate_customer_number(commit=False)
+
+    def save(self, **kwargs):
+        if not self.customer_number:
+            self.generate_customer_number(commit=False)
+        super(CustomerModel, self).save(**kwargs)
