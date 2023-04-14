43fe7ea21dd097c4c5bcad04efe74aba191c2a4d
Miguel Sanda <msanda@arrobalytics.com>
2023-04-14 15:09:02 -0400

    MixIns Documentation GraphQL Foundation Update

--- django_ledger/contrib/django_ledger_graphene/entity/schema.py
+++ django_ledger/contrib/django_ledger_graphene/entity/schema.py
@@ -29,12 +29,16 @@ class EntityModelType(DjangoObjectType):
         model = EntityModel
         fields = ENTITY_MODEL_BASE_FIELDS
         filter_fields = {
-            'name': ['exact', 'icontains', 'istartswith'],
+            'name': [
+                'exact',
+                'icontains',
+                'istartswith'
+            ],
         }
         interfaces = (relay.Node,)
 
 
-class EntityModelDetailNode(EntityModelType):
+class EntityModelTypeDetail(EntityModelType):
     class Meta:
         model = EntityModel
         fields = ENTITY_MODEL_BASE_FIELDS + [
@@ -43,14 +47,14 @@ class EntityModelDetailNode(EntityModelType):
 
 
 class EntityModelQuery(graphene.ObjectType):
-    all_entity_list = graphene.List(EntityModelType)
-    visible_entity_list = graphene.List(EntityModelType)
-    hidden_entity_list = graphene.List(EntityModelType)
-    managed_entity_list = graphene.List(EntityModelType)
-    admin_entity_list = graphene.List(EntityModelType)
+    entity_model_list_all = graphene.List(EntityModelType)
+    entity_model_list_visible = graphene.List(EntityModelType)
+    entity_model_list_hidden = graphene.List(EntityModelType)
+    entity_model_list_managed = graphene.List(EntityModelType)
+    entity_model_list_is_admin = graphene.List(EntityModelType)
 
-    entity_detail_by_uuid = graphene.Field(EntityModelDetailNode, uuid=graphene.String(required=True))
-    entity_detail_by_slug = graphene.Field(EntityModelDetailNode, slug=graphene.String(required=True))
+    entity_model_detail_by_uuid = graphene.Field(EntityModelTypeDetail, uuid=graphene.String(required=True))
+    entity_model_detail_by_slug = graphene.Field(EntityModelTypeDetail, slug=graphene.String(required=True))
 
     @staticmethod
     def get_base_queryset(info):
@@ -59,32 +63,32 @@ class EntityModelQuery(graphene.ObjectType):
         return EntityModel.objects.none()
 
     # list ....
-    def resolve_all_entity_list(self, info, **kwargs):
+    def resolve_entity_model_list_all(self, info, **kwargs):
         return EntityModelQuery.get_base_queryset(info)
 
-    def resolve_visible_entity_list(self, info, **kwargs):
+    def resolve_entity_model_list_visible(self, info, **kwargs):
         qs = EntityModelQuery.get_base_queryset(info)
         return qs.visible()
 
-    def resolve_hidden_entity_list(self, info, **kwargs):
+    def resolve_entity_model_list_hidden(self, info, **kwargs):
         qs = EntityModelQuery.get_base_queryset(info)
         return qs.hidden()
 
-    def resolve_managed_entity_list(self, info, **kwargs):
+    def resolve_entity_model_list_managed(self, info, **kwargs):
         qs: EntityModelQuerySet = EntityModelQuery.get_base_queryset(info)
         user_model = info.context.resource_owner
         return qs.filter(managers__in=[user_model])
 
-    def resolve_admin_entity_list(self, info, **kwargs):
+    def resolve_entity_model_list_is_admin(self, info, **kwargs):
         qs: EntityModelQuerySet = EntityModelQuery.get_base_queryset(info)
         user_model = info.context.resource_owner
         return qs.filter(admin=user_model)
 
     # detail...
-    def resolve_entity_detail_by_slug(self, info, slug, **kwargs):
+    def resolve_entity_model_detail_by_slug(self, info, slug, **kwargs):
         qs: EntityModelQuerySet = EntityModelQuery.get_base_queryset(info)
         return qs.select_related('default_coa').get(slug__exact=slug)
 
-    def resolve_entity_detail_by_uuid(self, info, uuid, **kwargs):
+    def resolve_entity_model_detail_by_uuid(self, info, uuid, **kwargs):
         qs: EntityModelQuerySet = EntityModelQuery.get_base_queryset(info)
         return qs.select_related('default_coa').get(uuid__exact=uuid)
