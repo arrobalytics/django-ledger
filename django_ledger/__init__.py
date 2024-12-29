eec1837ed31650791d4476c842c2a3314fc01a23
Miguel Sanda <msanda@arrobalytics.com>
2024-12-29 10:10:21 -0500

    Bump version to 0.7.2
    Update version in `pyproject.toml` and `__init__.py` to 0.7.2. This prepares the project for the new release by synchronizing version metadata.

--- django_ledger/__init__.py
+++ django_ledger/__init__.py
@@ -6,7 +6,7 @@ Copyright© EDMA Group Inc licensed under the GPLv3 Agreement.
 default_app_config = 'django_ledger.apps.DjangoLedgerConfig'
 
 """Django Ledger"""
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 __license__ = 'GPLv3 License'
 
 __author__ = 'Miguel Sanda'
