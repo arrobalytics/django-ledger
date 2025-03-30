a48db507e2f76f80fb145058f67bfe341f716142
George Petsagourakis <petsagouris@gmail.com>
2025-03-30 23:43:11 +0300

    ruff fixing trailing-whitespace (W291)

--- django_ledger/contrib/django_ledger_graphene/auth/mutations.py
+++ django_ledger/contrib/django_ledger_graphene/auth/mutations.py
@@ -9,14 +9,14 @@ from graphql_auth import mutations
 # class UserType(DjangoObjectType):
 #    class Meta:
 #       model = get_user_model()
-# 
+#
 # class CreateUser(graphene.Mutation):
 #    user = graphene.Field(UserType)
 #    class Arguments:
 #       username = graphene.String(required=True)
 #       password = graphene.String(required=True)
 #       email = graphene.String(required=True)
-# 
+#
 #    def mutate(self, info, username, password, email):
 #       user = get_user_model()(
 #          username=username,
@@ -25,7 +25,7 @@ from graphql_auth import mutations
 #       user.set_password(password)
 #       user.save()
 #       return CreateUser(user=user)
-# 
+#
 # class CreateUserMutation(graphene.ObjectType):
 #    create_user = CreateUser.Field()
 
