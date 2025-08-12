10830f8d18240a88624dd512beb606b903cf6849
ChayoteJarocho <77412126+ChayoteJarocho@users.noreply.github.com>
2025-08-12 13:28:25 -0700

    Add LOCALE_PATHS array to settings.py with the location of the globally shared locales folder.

--- dev_env/settings.py
+++ dev_env/settings.py
@@ -98,6 +98,8 @@ TIME_ZONE = 'America/New_York'
 USE_I18N = True
 USE_L10N = True
 
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
