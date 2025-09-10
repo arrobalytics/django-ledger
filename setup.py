386f415e163636064ee09bc169b539f7027a5f17
Miguel Sanda <msanda@arrobalytics.com>
2025-09-09 23:57:33 -0500

    setup.py deprecation.

--- setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup, find_namespace_packages
-
-PACKAGES = find_namespace_packages(exclude=["dev_env", "docs", "assets", "docs.source", "notebooks"])
-
-setup(packages=PACKAGES)
